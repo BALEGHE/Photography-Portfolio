06255368e936e3c24fb0e4356c919d20c332914a
BALEGHE <njoroge.austin@strathmore.edu>
2026-06-28 00:13:00 +0300

    Referenced all pages to this specific css file

---
+++ THU/src/App.tsx
@@ -0,0 +1,11 @@
+import Greetings from './context';
+
+function App() {
+  return (
+    <div>
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
---
+++ htdocs/README.md
@@ -0,0 +1,2 @@
+# Photography-Portfolio
+This is a repository for my photography Portfolio
